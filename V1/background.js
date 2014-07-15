0e1a101e2e10696261b54610c71097da8543aa62
pankaj saini <saini.info@gmail.com>
2014-07-15 18:14:35 +0530

    Fixed regular expression for chrome extension.

--- V1/background.js
+++
@@ -1,9 +0,0 @@
-chrome.tabs.onUpdated.addListener(function(tabId, change, tab) {
-  if (change.status == "complete") {
-    chrome.tabs.executeScript(null, {file: "content_script.js"}, function() {
-      if (chrome.runtime.lastError) {
-        console.error(chrome.runtime.lastError.message);
-      }
-    });
-  }
-});
--- phone_content_script.js
+++ phone_content_script.js
@@ -1,34 +1,34 @@
+//console.log(window);
 var hostname = document.location.hostname;
 if (window == top) {
   chrome.extension.onRequest.addListener(function(req, sender, sendResponse) {
     if (hostname.indexOf('pro.lookup.whitepages.com')== -1){
-    sendResponse(findAddress());
-	}
+      sendResponse(findAddress());
+    }
   });
-} 
+}
 
 // Search the text nodes for a US-style mailing address.
 // Return null if none is found.
-var findAddress = function() { 
-  var node = document.body; 
-   wpReversePhoneLookup(node);
+var findAddress = function() {
+  var node = document.body;
+  wpReversePhoneLookup(node);
 }
 
 
-function wpReversePhoneLookup(elem) {  
+function wpReversePhoneLookup(elem) {
   var nodes = elem.childNodes
    , i = nodes.length
-    //, regexp =  /\d*[-\s\.\(\ ][0-9][0-9][0-9][-\s\.\)\ ]*[0-9][0-9][0-9][-\s\.]*[0-9][0-9][0-9][0-9]*[(\.)|]*/gi
-   , regexp =  /((\(|( |))?[0-9]{3}([- . ]|\)|) ?([0-9]{3}([- . ]|)[0-9]{4}| [0-9]{3}[0-9]{4})|([+]|)?[1][- ]?[0-9]{3}([- . ]|\)|) ?[0-9]{3}[- . ][0-9]{4})/gi
+   , regexp =  /([0-9]{3})\)?[-. ]?[^0-9a-zA-Z]([0-9]{3})[-. ]?([0-9]{4})/g
    , node, phoneNode, a, result;
-  while (node = nodes[--i]) { 
-    if (node.nodeType === 1) {  
+  while (node = nodes[--i]) {
+    if (node.nodeType === 1) {
       wpReversePhoneLookup(node);
-    } else if (node.nodeType === 3) { 
-      while (result = regexp.exec(node.textContent)) { 
+    } else if (node.nodeType === 3) {
+      while (result = regexp.exec(node.textContent)) {
         node = node.splitText(result.index);
         node = node.splitText(result[0].length);
-        phoneNode = node.previousSibling 
+        phoneNode = node.previousSibling
         var link = "https://pro.lookup.whitepages.com/phones?number=" + result[0];
         var imgURL = chrome.extension.getURL("images/rsz_wp_16.png");
         var img = new Image();
@@ -44,15 +44,15 @@ function wpReversePhoneLookup(elem) {
         wpLink.className = "wpChromeExtension-link";
         //Append phoneNode
         wpLink.appendChild(img)
-        var refNode = phoneNode;  
-        if (node.nextSibling != null){ 
-		//console.log(node.nextElementSibling.nodeName); 
-		if (node.previousSibling == refNode && node.nextElementSibling.nodeName == "BR" ){ 
-		  refNode.parentNode.insertBefore(wpLink, refNode.nextSibling);
-		} 
-		if (node.previousSibling == refNode && node.nextElementSibling.nodeName == "A" && node.nextElementSibling.className != "wpChromeExtension-link" ){ 
-		  refNode.parentNode.insertBefore(wpLink, refNode.nextSibling);
-		 }	 
+        var refNode = phoneNode;
+        if (node.nextSibling != null){
+          //console.log(node.nextElementSibling.nodeName);
+          if (node.previousSibling == refNode && node.nextElementSibling.nodeName == "BR" ){
+            refNode.parentNode.insertBefore(wpLink, refNode.nextSibling);
+          }
+          if (node.previousSibling == refNode && node.nextElementSibling.nodeName == "A" && node.nextElementSibling.className != "wpChromeExtension-link" ){
+            refNode.parentNode.insertBefore(wpLink, refNode.nextSibling);
+          }
         }else
         if (node.nextSibling == null){
           refNode.parentNode.insertBefore(wpLink, refNode.nextSibling);
